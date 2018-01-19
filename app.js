6c2178de79c1a664f0ccb0d441b0fda4b9672e3e
akinmaurice <akinmaurice@gmail.com>
2018-01-19 17:34:00 +0100

    Added Cors

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const logger = require('morgan');
+const cors = require('cors');
 
 const routes = require('./routes/index');
 
 
 // GraphQL Schema
 import schema from './graphql';
+import { request } from 'http';
 
 
 const app = express();
 
-
+app.use(cors());
 app.use(logger('dev'));
 
 // Handle our routes!
